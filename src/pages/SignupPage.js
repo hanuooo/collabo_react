b77a094f08b40b3f1d1b1e78d35805c2d219f92f
allright <allright@example.com>
2025-09-19 16:07:24 +0900

    collabo react 2025/09/19

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,5 +1,9 @@
+import axios from "axios";
 import { useState } from "react";
-import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import { Alert, Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "../config/config";
+
 
 function App(){
     // 파라미터 관련 state 변수 선언
@@ -7,6 +11,49 @@ function App(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [address, setAddress] = useState('');
+
+    // Form 유효성 검사(Form Validation Check) 관련 state 정의 : 입력 양식에 문제 발생시 값을 저장할 곳
+    const[errors, setErrors] = useState({
+        name : '', email:'', password:'', address:'', general:''
+
+    });
+
+        const navigate =useNavigate();
+        
+       /*
+        구분        async/await 사용           then/catch 사용
+        필수 여부    없어도 됨                   가능
+        가독성    더 깔끔                       체인이 길어지면 복잡
+        에러 처리    try...catch 한 번에 가능       .catch() 따로 작성
+        추천 여부    대부분의 비동기 코드에서 추천   간단한 한 줄짜리 Promise라면 가능
+        */
+
+    const SignupAction = async (event) => {
+
+        event.preventDefault(); // 이벤트 전파방지
+
+        try{
+            const url = `${API_BASE_URL}/member/signup`;
+            const parameters = {name, email, password, address};
+
+            // response는 응답 받은 객체입니다.
+            const response = await axios.post(url, parameters);
+            
+            if(response.status === 200){ /* 스프링의 MemberController 파일 참조 */
+                alert('회원 가입 성공')
+                navigate('/member/login')
+            }
+        
+        }catch(error){ // error : 예외 객체
+            if(error.response && error.response.data){
+                // 서버에서 받은 오류 정보를 객체로 저장합니다.
+                setErrors(error.response.data);
+            }else{ // 입력 값 이외에 발생하는 다른 오류와 관련됨
+                setErrors((previous)=>({...previous, general : '회원 가입 중에 오류가 발생하였습니다.'}));
+            }
+        }
+    };
+
     return(
                 <Container className="d-flex justify-content-center align-items-center" style={{ height: '70vh' }}>
             <Row className="w-100 justify-content-center">
@@ -14,7 +61,19 @@ function App(){
                     <Card>
                         <Card.Body>
                             <h2 className="text-center mb-4">회원가입</h2>
-                        <Form>
+
+                            {/* 일반 오류 발생시 사용자에게 alert 메시지를 보여 줍니다. */}
+                            {/* contextual : 상황에 맞는 적절한 색상을 지정하는 기법 */}
+                            {errors.general && <Alert variant="danger">{errors.general}</Alert>}      
+
+                            {/* 
+                                !! 연산자는 어떠한 값을 강제로 boolean 형태로 변환해주는 자바스크립트 기법입니다.
+
+                                isInvalid 속성은 해당 control의 유효성을 검사하는 속성입니다.
+                                값이 true이면 Form.Control.Feedback에 빨간 색상으로 오류 메시지를 보여 줍니다.
+                            */}                     
+
+                        <Form onSubmit={SignupAction}>
                             <Form.Group className="mb-3">
                                 <Form.Label>이름</Form.Label>
                                 <Form.Control 
@@ -22,7 +81,11 @@ function App(){
                                     placeholder="이름을 입력해 주세요."
                                     value={name}
                                     onChange={(event)=>setName(event.target.value)}
+                                    isInvalid={!!errors.name}
                                 />
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.name}
+                                    </Form.Control.Feedback>
                             </Form.Group>
 
                                <Form.Group className="mb-3">
@@ -33,7 +96,11 @@ function App(){
                                     value={email}
                                     onChange={(event)=>setEmail(event.target.value)}
                                     required // 필수 입력 항목
+                                    isInvalid={!!errors.email}
                                 />
+                                 <Form.Control.Feedback type="invalid">
+                                    {errors.email}
+                                    </Form.Control.Feedback>
                             </Form.Group>
 
                                <Form.Group className="mb-3">
@@ -44,7 +111,11 @@ function App(){
                                     value={password}
                                     onChange={(event)=>setPassword(event.target.value)}
                                     required
+                                    isInvalid={!!errors.password}
                                 />
+                                 <Form.Control.Feedback type="invalid">
+                                    {errors.password}
+                                    </Form.Control.Feedback>
                             </Form.Group>
 
                                <Form.Group className="mb-3">
@@ -55,7 +126,11 @@ function App(){
                                     value={address}
                                     onChange={(event)=>setAddress(event.target.value)}
                                     required
+                                    isInvalid={!!errors.address}
                                 />
+                                 <Form.Control.Feedback type="invalid">
+                                    {errors.address}
+                                    </Form.Control.Feedback>
                             </Form.Group>
                                 <Button variant="primary" type="submit" className="w-100">
                                     회원 가입
