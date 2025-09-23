6c8d1c95d90a87c7b5a8284fbc6905884201139c
allright <allright@example.com>
2025-09-23 16:51:48 +0900

    collabo react 2025/09/23

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
 
 // 분리된 리액트 컴포넌트 import
-import MenuItems from './ui/MenuItems' ;
-import AppRoutes from './routes/AppRoutes' ;
+import MenuItems from './ui/MenuItems';
+import AppRoutes from './routes/AppRoutes';
 
 import { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { API_BASE_URL } from './config/config';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const appName = "IT Acadeny Coffee Shop";
+  const appName = "IT Academy Coffee Shop";
 
   // user : 로그인 한 사람의 정보를 저장하고 있는 state
-  // 클라이언트 사용자 정보를 저장하기 위항 localStorage를 사용하겠습니다.
+  // 클라이언트에서 사용자 정보를 저장하기 위하여 localStorage를 사용하겠습니다.
   const [user, setUser] = useState(null);
 
   // JSON.parse()는 JSON 형태의 문자열을 자바 스크립트 객체 형태로 변환해 줍니다.
-  useEffect (() => {
+  useEffect(() => {
     const loginUser = localStorage.getItem('user');
     setUser(JSON.parse(loginUser));
   }, []); // 2번째 매개 변수가 empty array이므로 1번만 rendering됩니다.
@@ -26,20 +29,27 @@ function App() {
     console.log('로그인 성공');
   }
 
-  const handleLogoutSuccess = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = (event) => {
+    event.preventDefault();
+
     setUser(null);
-  }
+    localStorage.removeItem('user');
+    console.log('로그 아웃 성공');
+    navigate(`/member/login`);
+  };
 
   return (
     <>
-          <MenuItems appName={appName} user={user} />
-     
-     {/* 분리된 라우터 정보 */}
-     <AppRoutes user={user} handleLoginSuccess={handleLoginSuccess} logout={handleLogoutSuccess} />
-      
-     <footer className="bg-dark text-light text-center py-3 mt-5">
-      <p>&copy; 2025 {appName}. ALL rights reserved.</p>
-     </footer>
+      <MenuItems appName={appName} user={user} handleLogout={handleLogout} />
+
+      {/* 분리된 라우터 정보 */}
+      <AppRoutes user={user} handleLoginSuccess={handleLoginSuccess} />
+
+      <footer className="bg-dark text-light text-center py-3 mt-5">
+        <p>&copy; 2025 {appName}. All rights reserved.</p>
+      </footer>
     </>
   );
 }
